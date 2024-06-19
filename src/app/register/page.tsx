f736c9f29551bbc61efc886becf9e7dc48e5cccd
unknown <watsapol.ping@gmail.com>
2024-06-19 12:54:39 +0700

    implement login and register ui page

---
+++ src/app/register/page.tsx
@@ -0,0 +1,71 @@
+"use client";
+
+import React from "react";
+import { Layout } from "antd";
+import Navbar from "../components/Navbar";
+import Link from "next/link";
+import FooterCompo from "../components/FooterCompo";
+
+const RegisterPage = () => {
+  const { Content, Footer } = Layout;
+
+  return (
+    <Layout>
+      <Navbar />
+      <Content>
+        <div className="flex-grow">
+          <div className="flex justify-center items-center">
+            <div className="w-[400px] shadow-xl p-10 mt-5 rounded-xl bg-slate-500">
+              <h3 className="text-3xl text-black">Register</h3>
+              <hr className="my-3" />
+
+              <form action="">
+                <input
+                  type="text"
+                  className="w-full bg-gray-200 border py-2 px-3 rounded text-lg my-2"
+                  placeholder="Enter your name"
+                />
+                <input
+                  type="email"
+                  className="w-full bg-gray-200 border py-2 px-3 rounded text-lg my-2"
+                  placeholder="Enter your email"
+                />
+                <input
+                  type="password"
+                  className="w-full bg-gray-200 border py-2 px-3 rounded text-lg my-2"
+                  placeholder="Enter your Password"
+                />
+                <input
+                  type="password"
+                  className="w-full bg-gray-200 border py-2 px-3 rounded text-lg my-2"
+                  placeholder="Confirm your password"
+                />
+                <button
+                  className="bg-green-500 text-white border py-2 px-3 rounded text-lg my-2"
+                  type="submit"
+                >
+                  Sign Up
+                </button>
+                <hr className="my-3" />
+
+                <p>
+                  Already have an acount? Go to{" "}
+                  <Link
+                    href="/login"
+                    className="text-blue-200 hover:underline"
+                  >
+                    Login
+                  </Link>{" "}
+                  Page
+                </p>
+              </form>
+            </div>
+          </div>
+        </div>
+      </Content>
+      <FooterCompo />
+    </Layout>
+  );
+};
+
+export default RegisterPage;
