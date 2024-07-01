7d01fcaa928f898af66a48e996c1c1ab9890e5fe
unknown <watsapol.ping@gmail.com>
2024-07-01 16:29:43 +0700

    refactor: 💡 create dataTable for admin

--- src/app/admin/posts/page.tsx
+++ src/app/admin/posts/page.tsx
@@ -1,31 +1,111 @@
-'use client'
+"use client";
 
-import { AllPostsType } from '@/app/welcome/page'
-import React, { useEffect, useState } from 'react'
-import PostTable from '@/components/admin/PostTable'
-import { NextPage } from 'next'
+import { AllPostsType } from "@/app/welcome/page";
+import React, { useEffect, useState } from "react";
+import { NextPage } from "next";
 
-import { LocalStorage } from '@/utils/getData'
+import { LocalStorage } from "@/utils/getData";
+import DataTable from "@/components/admin/DataTable";
+import { Button, Popconfirm, Space, TableProps } from "antd";
+import { useRouter } from "next/navigation";
+import { delayTimeout } from "@/utils/dalay";
+
+import Image from 'next/image'
+
+export interface PostTableType {
+  id: number
+  title: string
+  imageUrl: string
+  description: string
+}
 
 const ManagePostPage: NextPage = () => {
+  const [postData, setPostData] = useState<AllPostsType[]>([]);
 
-  const [postData, setPostData] = useState<AllPostsType[]>([])
+  const route = useRouter();
+  const [confirmLoading, setConfirmLoading] = useState(false);
 
-  useEffect(()=> {
-    const loadPost = () => {
-      const posts = LocalStorage().getPost()
-      setPostData(posts)
+  const handleNaviEdit = (id: number) => {
+    route.push(`/admin/posts/${id}`)
+  }
+
+  const handleConfirmPopup = async (postId: number) => {
+    setConfirmLoading(true)
+
+    if(postData) {
+      const updateDataSource = postData.filter((data: PostTableType) => data.id !== postId)
+
+      await delayTimeout(1000)
+
+      localStorage.setItem('posts', JSON.stringify(updateDataSource))
+        setConfirmLoading(false)
+        window.location.reload()
+
     }
+  }
 
-    loadPost()
-  }, []) 
+  useEffect(() => {
+    const loadPost = () => {
+      const posts = LocalStorage().getPost();
+      setPostData(posts);
+    };
+
+    loadPost();
+  }, []);
+
+  const columns: TableProps<PostTableType>['columns'] = [
+    {
+      title: 'ID',
+      dataIndex: 'id',
+      key: 'id'
+    },
+    {
+      title: 'title',
+      dataIndex: 'title',
+      key: 'title'
+    },
+    {
+      title: 'Image Url',
+      dataIndex: 'imageUrl',
+      key: 'imageUrl',
+      render: (_, record) => (
+        <Image src={record.imageUrl} height={100} width={100} alt='user image'/>
+      ),
+    },
+    {
+      title: 'Description',
+      dataIndex: 'description',
+      key: 'description'
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (_, record) => (
+        <Space>
+          <Button type='primary' onClick={()=> handleNaviEdit(record.id)}>Edit</Button>
+          <Popconfirm
+            title='You want to delete this post?'
+            okButtonProps={{ loading: confirmLoading }}
+            onConfirm={()=> handleConfirmPopup(record.id)}
+          >
+            <Button 
+              danger 
+              type='primary'
+            >
+              Delete
+            </Button>
+          </Popconfirm>
+        </Space>
+      )
+    }
+  ]
 
   return (
     <section>
-      <h1 className='text-3xl mb-5'>Manage Posts</h1>
-      <PostTable dataSource={postData}/>
+      <h1 className="text-3xl mb-5">Manage Posts</h1>
+      <DataTable columns={columns} dataSource={postData} />
     </section>
-  )
-}
+  );
+};
 
-export default ManagePostPage
+export default ManagePostPage;
--- src/app/admin/users/page.tsx
+++ src/app/admin/users/page.tsx
@@ -1,13 +1,93 @@
 'use client'
 
 import React, { useEffect, useState } from 'react'
-import UserTable, { TableType } from '@/components/admin/UserTable'
 import { NextPage } from 'next'
 
 import { LocalStorage } from '@/utils/getData'
+import DataTable from '@/components/admin/DataTable'
+import { Button, Popconfirm, Space, TableProps } from 'antd'
+import { useRouter } from 'next/navigation'
+import { delayTimeout } from '@/utils/dalay'
+
+export interface UserTableType {
+  id: number;
+  userName: string;
+  email: string;
+  password: string;
+}
 
 const ManageUserPage: NextPage = () => {
-  const [userData, setUserData] = useState<TableType[]>([])
+  const [userData, setUserData] = useState<UserTableType[]>([])
+
+  const route = useRouter();
+  const [confirmLoading, setConfirmLoading] = useState(false);
+
+  const handleNavigateToEdit = (id: number) => {
+    route.push(`/admin/users/${id}`);
+  };
+
+  const handleOkPopupConfirm = async (userId: number) => {
+    setConfirmLoading(true);
+
+    const updateDataSource = userData.filter(
+      (data: UserTableType) => data.id !== userId
+    );
+
+    await delayTimeout(500)
+
+    localStorage.setItem("userData", JSON.stringify(updateDataSource));
+
+    setConfirmLoading(false);
+    window.location.reload();
+
+  };
+
+  const columns: TableProps<UserTableType>["columns"] = [
+    {
+      title: "ID",
+      dataIndex: "id",
+      key: "id",
+    },
+    {
+      title: "username",
+      dataIndex: "userName",
+      key: "userName",
+    },
+    {
+      title: "email",
+      dataIndex: "email",
+      key: "email",
+    },
+    {
+      title: "password",
+      dataIndex: "password",
+      key: "password",
+    },
+    {
+      title: "Action",
+      key: "action",
+      render: (_, record) => (
+        <Space>
+          <Button type="primary" onClick={() => handleNavigateToEdit(record.id)}>
+            Edit
+          </Button>
+          <Popconfirm
+            title="You want to delete this user?"
+            okButtonProps={{ loading: confirmLoading }}
+            onConfirm={() => handleOkPopupConfirm(record.id)}
+          >
+            <Button
+              danger
+              type="primary"
+            >
+              Delete
+            </Button>
+          </Popconfirm>
+        </Space>
+      ),
+    },
+  ];
+
 
   useEffect(() => {
     const users = LocalStorage().getUsers()
@@ -17,7 +97,10 @@ const ManageUserPage: NextPage = () => {
   return (
     <section>
       <h1 className='text-3xl mb-5'>Manage Users</h1>
-      <UserTable dataSource={userData}/>
+      <DataTable 
+        columns={columns}
+        dataSource={userData}
+      />
     </section>
   )
 }
